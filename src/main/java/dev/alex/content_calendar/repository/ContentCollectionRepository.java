2569463cf085acac82da73ef53e4a00e17212a0c
alexblack24 <alex.black@mail.com>
2024-09-29 23:19:28 +0200

    Added some data to view on the response web page.

--- src/main/java/dev/alex/content_calendar/repository/ContentCollectionRepository.java
+++ src/main/java/dev/alex/content_calendar/repository/ContentCollectionRepository.java
@@ -1,8 +1,12 @@
 package dev.alex.content_calendar.repository;
 
 import dev.alex.content_calendar.model.Content;
+import dev.alex.content_calendar.model.Status;
+import dev.alex.content_calendar.model.Type;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -19,5 +23,11 @@ public class ContentCollectionRepository {
     }
     public Optional<Content> findById(Integer id) {
         return content.stream().filter(c -> c.id().equals(id)).findFirst();
+    }
+@PostConstruct
+    private void init() {
+        Content c = new Content(1, "My First Title", "My First Description", Status.IDEA, Type.ARTICLE, LocalDate.now(), null, "https://example.com");
+
+        content.add(c);
     }
 }
